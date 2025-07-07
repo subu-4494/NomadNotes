3dbd7522315691bcd7c7a49610a27cf6d15cfe1a
Sourabh <sourabroy2018@gmail.com>
2025-07-08 04:22:20 +0530

    updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://nomadnotes-backend.onrender.com/api";
 
 // LOGIN
 export async function loginUser(credentials) {
