a85c95882445d8e0cda9c4599419192c8dc78bdc
Sourabh <sourabroy2018@gmail.com>
2025-07-08 04:05:14 +0530

    update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -27,13 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  });
-}
 
 // Connect MongoDB and start server
 mongoose.connect(process.env.MONGO_URI)
